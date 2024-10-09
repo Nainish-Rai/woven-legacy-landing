eb4cb85842243a72b76c1f8fd929a330441ae1dc
Nainish Rai <nainishraisjp@gmail.com>
2024-10-10 00:59:13 +0530

    fixed names

--- throwCollection.ts
+++ throwCollection.ts
@@ -9,41 +9,41 @@ export const throwCollection = [
           "/UNI - A (2) - 100 _ Viscose.jpg",
         ],
       },
-      {
-        title: "UNI - B - 100% Viscose",
-        image: [
-          "/UNI - B (3) - 100 _ Viscose.jpg",
-          "/UNI - B (2) - 100 _ Viscose.jpg",
-        ],
-      },
-      {
-        title: "UNI - C - 100% Viscose",
-        image: [
-          "/UNI - C (3) - 100 _ Viscose .jpg",
-          "/UNI - C (2) - 100 _ Viscose .jpg",
-        ],
-      },
-      {
-        title: "UNI - D - 100% Viscose",
-        image: [
-          "/UNI - D (3) - 100 _ Viscose.jpg",
-          "/UNI - D (2) - 100 _ Viscose.jpg",
-        ],
-      },
-      {
-        title: "UNI - E - 100% Viscose",
-        image: [
-          "/UNI - E (2) - 100 _ Viscose.jpg",
-          "/UNI - E (3) - 100 _ Viscose.jpg",
-        ],
-      },
+      // {
+      //   title: "UNI - B - 100% Viscose",
+      //   image: [
+      //     "/UNI - B (3) - 100 _ Viscose.jpg",
+      //     "/UNI - B (2) - 100 _ Viscose.jpg",
+      //   ],
+      // },
+      // {
+      //   title: "UNI - C - 100% Viscose",
+      //   image: [
+      //     "/UNI - C (3) - 100 _ Viscose .jpg",
+      //     "/UNI - C (2) - 100 _ Viscose .jpg",
+      //   ],
+      // },
+      // {
+      //   title: "UNI - D - 100% Viscose",
+      //   image: [
+      //     "/UNI - D (3) - 100 _ Viscose.jpg",
+      //     "/UNI - D (2) - 100 _ Viscose.jpg",
+      //   ],
+      // },
+      // {
+      //   title: "UNI - E - 100% Viscose",
+      //   image: [
+      //     "/UNI - E (2) - 100 _ Viscose.jpg",
+      //     "/UNI - E (3) - 100 _ Viscose.jpg",
+      //   ],
+      // },
     ],
   },
   {
     name: "Mike Collection",
     products: [
       {
-        title: "MIKE - Hand Tufted - Viscose, Cotton and Linen",
+        title: "Mike - 70 % Viscose + 15 % Cotton + 15 % Linen",
         image: [
           "/MIKE  - VISCOSE, COTTON AND LINEN (2).jpg",
           "/MIKE  - VISCOSE, COTTON AND LINEN (1).jpg",
@@ -55,7 +55,7 @@ export const throwCollection = [
     name: "Rais Collection",
     products: [
       {
-        title: "RAIS - 100 - Cotton ",
+        title: "Rais - 100% Polyester ",
         image: ["/RAIS - 100 - (2).jpg", "/RAIS - 100 - (1).jpg"],
       },
     ],
@@ -64,7 +64,7 @@ export const throwCollection = [
     name: "Steve Collection",
     products: [
       {
-        title: "STEVE - 70 - Polyester ",
+        title: "Steve - 55 % Cotton + 35 % Viscose + 10 % Linen ",
         image: ["/STEVE - 70 - (1).jpg", "/STEVE - 70 - (2).jpg"],
       },
     ],
